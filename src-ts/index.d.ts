963651faeba6f06c2daefb17a8fb53467dbdfc28
Dawid Tomkalski <demsey2@gmail.com>
2021-07-23 17:00:34 +1000

    remove ts

--- src-ts/index.d.ts
+++
@@ -1,8 +0,0 @@
-/*
-    dynamodb-onetable - DynamoDB wrapper for single table patterns
-*/
-
-import { AnyEntity, AnyModel, Entity, Model, OneParams, OneProperties, Paged } from './Model'
-import { Table } from './Table'
-
-export { AnyEntity, AnyModel, Entity, Model, OneParams, OneProperties, Paged, Table }
